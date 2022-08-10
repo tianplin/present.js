d6aca844579c1cedc35b1dc7a825fe911254010c
陈天林 <1256860196@qq.com>
2022-08-10 17:18:31 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
-import { Presentjs } from './present'
+import { Presentjs } from './present.js'
 
 const isPresentjs = (p) => p instanceof Presentjs //是否是Presentjs实例
 
-const presentjs = function (data, c) {}
+export const presentjs = function (data, c) {
+  debugger
+  if (isPresentjs(data)) {
+    return data.clone() //如果是presentjs对象，则返回一个新的presentjs对象
+  }
+  const config = typeof c == 'object' ? c : {}
+  config.date = data //设置日期
+  config.args = arguments //设置参数
+  return new Presentjs(config) //返回一个新的presentjs对象
+}
